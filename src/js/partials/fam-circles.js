003eb7a3afd3e473e8537ac3e5d328610e283625
Nate Druss <nate@nsideas.com>
2018-06-06 15:00:19 -0400

    Fam circle resize function

--- src/js/partials/fam-circles.js
+++ src/js/partials/fam-circles.js
@@ -9,6 +9,7 @@ var big_circle = 620;
 var margin = 10;
 var t = small_circle/2 + big_circle/2 + margin;
 var swiper_is_active = false;
+
 if (fam_circles) {
   slides = fam_circles.querySelectorAll('.swiper-slide');
 }
@@ -25,42 +26,45 @@ function get_slide_offset() {
          : -small_circle/2
 }
 
-var fam_circle_swiper = new Swiper('.fam-circle-swiper', {
-  slidesPerView: 'auto',
-  roundLengths: true,
-  centeredSlides: true,
-  initialSlide: get_middle_slide(),
-  slidesOffsetBefore: get_slide_offset(),
-  virtualTranslate: true,
-  init: false,
-  longSwipes: false,
-  breakpoints: {
-    // when window width is <= 719px
-    719: {
-      freeMode: true,
-      freeModeMomentumRatio: 0.5,
-      freeModeMomentumVelocityRatio: 0.75,
-      slidesOffsetBefore: 5,
-      slidesOffsetAfter: 20,
-      allowTouchMove: true,
-      centeredSlides: false,
-      initialSlide: 0,
-      virtualTranslate: false,
-      init: true
+function swiper_settings() {
+  return {
+    slidesPerView: 'auto',
+    roundLengths: true,
+    centeredSlides: true,
+    initialSlide: get_middle_slide(),
+    slidesOffsetBefore: get_slide_offset(),
+    virtualTranslate: true,
+    init: false,
+    longSwipes: false,
+    breakpoints: {
+      // when window width is <= 719px
+      719: {
+        freeMode: true,
+        freeModeMomentumRatio: 0.5,
+        freeModeMomentumVelocityRatio: 0.75,
+        slidesOffsetBefore: 5,
+        slidesOffsetAfter: 20,
+        allowTouchMove: true,
+        centeredSlides: false,
+        initialSlide: 0,
+        virtualTranslate: false,
+        init: true
+      }
     }
   }
-});
 
-
-
-function slide_to(slide) {
-  var trans_value = slides[slide].getAttribute('data-translate');
-  translate_circles(trans_value);
 }
+var fam_circle_swiper = new Swiper('.fam-circle-swiper', swiper_settings());
+
 
 function translate_circles(x) {
   $('.fam-circle-swiper .swiper-wrapper').css('transform', 'translate3d('+ x +'px, 0px, 0px)');
   current_position = x;
+}
+
+function slide_to(slide) {
+  var trans_value = slides[slide].getAttribute('data-translate');
+  translate_circles(trans_value);
 }
 
 function get_translate_values(start) {
@@ -69,6 +73,12 @@ function get_translate_values(start) {
   for (var i = 0; i < slides.length; i++) {
     trans_value = (small_circle + margin) * (middle - i) + start - get_slide_offset();
     slides[i].setAttribute('data-translate', trans_value);
+  }
+}
+
+function fam_circle_slide_change(swiper) {
+  if (swiper_is_active) {
+    slide_to(swiper.activeIndex);
   }
 }
 
@@ -81,74 +91,84 @@ function initialize_swiper_manually() {
   $('.fam-circle-swiper').addClass('initialized');
 
   start_position = current_position - big_circle/2 + small_circle/2;
-  get_translate_values(start_position);
+
+  if (swiper_is_active) {
+    get_translate_values(current_translate);
+  } else {
+    get_translate_values(start_position);
+  }
+
   fam_circle_swiper.setTransition(300);
 
-  // Wait one second and fade in circles
-  setTimeout(function(){
-    $('.fam-circle-swiper').addClass('active');
-    if (slides.length % 2 > 0) {
-      translate_circles(start_position);
-    } else {
-      translate_circles(start_position - get_slide_offset());
-    }
-
-    swiper_is_active = true;
-  }, 1000);
+  if (!swiper_is_active) {
+    // Wait one second and fade in circles
+    setTimeout(function(){
+      $('.fam-circle-swiper').addClass('active');
+      if (slides.length % 2 > 0) {
+        translate_circles(start_position);
+      } else {
+        translate_circles(start_position - get_slide_offset());
+      }
+      swiper_is_active = true;
+    }, 1000);
+  }
 
 }
-
 
 
 var touch_start_position;
 var touched_index;
 
-if (mq_medium.matches) {
+function fam_circle_start() {
 
-  fam_circle_swiper.on('init', function() {
-    initialize_swiper_manually();
-  });
+  if (mq_medium.matches) {
 
-  fam_circle_swiper.on('slideChangeTransitionStart', function() {
-    fam_circle_slide_change(this);
-  });
+    fam_circle_swiper.on('init', function() {
+      initialize_swiper_manually();
+    });
 
-  fam_circle_swiper.on('touchStart', function(e) {
-    touch_start_position = e.x;
-    touched_index = this.activeIndex;
-  });
+    fam_circle_swiper.on('slideChangeTransitionStart', function() {
+      fam_circle_slide_change(this);
+    });
 
-  fam_circle_swiper.on('touchEnd', function(e) {
-    if (e.x < touch_start_position && !this.isEnd) {
-      this.slideTo(touched_index + 1, 300);
-    } else if (e.x > touch_start_position && !this.isBeginning) {
-      this.slideTo(touched_index - 1, 300);
-    }
-  });
+    fam_circle_swiper.on('touchStart', function(e) {
+      touch_start_position = e.x;
+      touched_index = this.activeIndex;
+    });
+
+    fam_circle_swiper.on('touchEnd', function(e) {
+      if (e.x < touch_start_position && !this.isEnd) {
+        this.slideTo(touched_index + 1, 300);
+      } else if (e.x > touch_start_position && !this.isBeginning) {
+        this.slideTo(touched_index - 1, 300);
+      }
+    });
+
+    $('.fam-circle-swiper .swiper-slide').click(function() {
+      var clicked_index = $(this).index();
+      if (clicked_index !== fam_circle_swiper.activeIndex) {
+        fam_circle_swiper.slideTo(clicked_index, 300);
+      }
+    });
+
+  }
+
+  fam_circle_swiper.init();
+  fam_circle_swiper.on('resize', debounce(swiper_resize_handler, 200));
 
 }
 
-
-function fam_circle_slide_change(swiper) {
-  if (swiper_is_active) {
-    slide_to(swiper.activeIndex);
-  }
+function swiper_resize_handler() {
+  fam_circle_swiper.destroy(false);
+  $('.fam-circle-swiper .swiper-wrapper').attr('style', '');
+  current_position = 0;
+  fam_circle_swiper = new Swiper('.fam-circle-swiper', swiper_settings());
+  fam_circle_start();
 }
 
 
 $(document).ready(function() {
   if (fam_circles) {
-
-    fam_circle_swiper.init();
-
-    if (mq_medium.matches) {
-      $('.fam-circle-swiper .swiper-slide').click(function() {
-        var clicked_index = $(this).index();
-        if (clicked_index !== fam_circle_swiper.activeIndex) {
-          fam_circle_swiper.slideTo(clicked_index, 300);
-        }
-      });
-    }
-
+    fam_circle_start();
   }
 });
