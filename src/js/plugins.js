5090ee67b94246538163441846c93aabb3653fdf
Nate Druss <nate@nsideas.com>
2018-06-06 14:24:43 -0400

    Added debounce function

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -21,6 +21,7 @@
     }
 }());
 
+// Throttle
 function throttle(fn, wait) {
   var time = Date.now();
   return function() {
@@ -29,6 +30,25 @@ function throttle(fn, wait) {
       time = Date.now();
     }
   }
+}
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
 }
 
 // Place any jQuery/helper plugins in here.
